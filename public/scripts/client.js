85b24cbf360a3adca2ef862cf36688ce9d6a95b0
Roxanne Charchian <rmcharchian@gmail.com>
2017-08-19 09:39:44 -0500

    input forms added to DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,11 @@
 console.log('client.js has been loaded');
+
+var app = angular.module('EmployeeApp', []);
+
+app.controller('EmployeeController', ['$http', function ($http){
+    console.log('Employee Controller has been loaded');
+    var self = this;
+    self.employees = [];
+
+
+}])
